78f41f2c9fc8ddcf8b11db0bb25517cf34484258
Roman Ivashkevich <roman.ivashkevich@hypernovateam.com>
2020-11-05 18:33:44 +0300

    add_homework_3

---
+++ script/js.js
@@ -0,0 +1,54 @@
+//1
+
+let a = 1;
+while (a <=50) {
+    console.log(a);
+    a++;
+}
+
+let b = 35;
+while (b >=8) {
+    console.log(b);
+    b--;
+}
+
+//2
+
+let c = 89;
+while (c >= 11) {
+    document.write(c, '<br>');
+    c--;
+}
+
+//3
+
+let sum = 0;
+    for(let i=0; i <=100; i++) {
+        sum += i;
+        
+    }
+    console.log(sum);
+
+//4
+
+let sum1 = 0;
+    for(let i=1; i <=5; i++) {
+        sum1 += i;
+        console.log(sum1);
+    }
+
+//5
+
+for (let t = 8; t <= 56; t++) {
+    if (t % 2 == 0) {
+        console.log(t)
+    }
+}
+
+let r = 8;
+while (r <= 56) {
+    if (r % 2 == 0) {
+        console.log(r)
+    }
+    r++
+}
